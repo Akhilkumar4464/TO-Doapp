746af0231faf45fbe600533973975144af17b573
Akhilkumar4464 <niteshsharma4464@gmail.com>
2025-06-24 23:49:06 +0530

    ""'

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,30 @@
+import React, { useState } from 'react';
 
+export default function App() {
+  const [title , setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  return (
+    <div className=' '>
+       <input type="
+       text" 
+        value={title}
+        onChange={(e) =>
+          setTitle(e.target.value)
+          }
+           placeholder='Enter your description' 
+
+       
+       />
+
+        <input type="
+    text"
+    className='w-full h-5 bg-blue-300 '
+     value={ description}
+     onChange={(e) =>
+      setDescription(e.target.value)
+     }
+     placeholder='Enter your description'
+    />
+    </div>
+  );
+}
